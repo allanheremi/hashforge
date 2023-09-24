fe5cf9e58471557f3d828d7d1c336a4fb3eafd9f
Allan Heremi <allanheremidev@gmail.com>
2023-09-24 17:39:43 +0200

    Fix: favicon

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,4 @@
-import Image from 'next/image';
 import Header from './components/Header';
-import InputField from './components/InputField';
-import HashList from './components/HashList';
 import Main from './components/Main';
 export default function Home() {
   return (
